0d5859355c6e69ec3b65ae8ae84716c79a8ed021
dhineshdini05-cyber <dhineshdini05@gmail.com>
2026-01-31 21:51:58 +0530

    Built web
    Branch: main
    Commit: 0b01f5e1ed4cdd373ed1716a5bd6d8a69091f6bb (dirty)
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ca3c4d489142bf6a82dea89f0f01106c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "e9ee0ec0232919f1f921227238045fcd",
+"flutter_bootstrap.js": "5a807e8d757b2a1f198827a08efb10fd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
